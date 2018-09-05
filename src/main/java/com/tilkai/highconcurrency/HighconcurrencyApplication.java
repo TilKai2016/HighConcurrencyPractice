1d440615ca67c54c60cfb962b1f76a19defc6804
chilonghai <chilonghai@thsolar.com>
2018-09-05 21:54:02 +0800

    initialize program.

---
+++ src/main/java/com/tilkai/highconcurrency/HighconcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.tilkai.highconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighconcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighconcurrencyApplication.class, args);
+    }
+}
